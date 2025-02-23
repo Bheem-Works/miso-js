ef9a1bb4b51f7bf491fab762259641eba11ba9f0
Bhim Magar <shokokomichan098@gmail.com>
2025-02-24 04:04:22 +0545

    changes commit

--- N.inherintence.js
+++ N.inherintence.js
@@ -1,23 +1,26 @@
-class Animal {
-  constructor(name) {
-    this.name = name;
-  }
-  meow() {
-    console.log(`${this.name} is hungry `);
-  }
-}
+// writing inheritance in the js 
 
-class Dog extends Animal {
-  constructor(name, breed) {
-    super(name); //calling the parent's constructor
-    this.breed = breed;
-  }
-  bark() {
-    console.log(`${this.name} is barking`);
-  }
+class Kim {
+    constructor(name,food) {
+      this.name = name;
+      this.food = food;
+    }
+    eat () {
+      return `${this.name} Likes ${this.food} So much `;
+    }
 }
+ class Time extends Kim {
+    constructor(name,food,time){
+      super(name,food);
+      this.time = time;
+    }
+    
+    like () {
+      return `${this.name} Eat at ${this.time} and ${this.food}He likes it so much`;
+    }
+ }
 
-// objects banune kaam
-const myDog = new Dog("Max", "Golden Retriever");
-myDog.meow();
-myDog.bark();
+ const a = new Kim ("Kim Miso ", "Eggs");
+ const b = new Time ("Kim Miso", 10, "Eggs");
+
+ console.log(b.like());
--- N.todoList.js
+++ N.todoList.js
@@ -22,3 +22,4 @@ function addItem(e) {
 
   itemsLi.appendChild(li);
 }
+ 
