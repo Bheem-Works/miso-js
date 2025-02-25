d951cc2ff1265024b73432ed59eed32cffd70f8f
Bhim Magar <shokokomichan098@gmail.com>
2025-02-26 05:35:37 +0545

    Math commit

---
+++ Q-Math.js
@@ -0,0 +1,25 @@
+    // Math in the js : Why the math object in javascript is useful?
+    // => because it collects the collections of the properties and methods for performing the mathematical tasks and calculation.
+
+    console.log(Math.PI); // it remains constant
+    console.log(Math.E); // it also remains the constant
+    
+    let s = 5;
+    let x = 2.544;
+    let y = 3;
+    let z;
+    // z = Math.floor(x); // gives the down numbers nearest to the integer
+    // z = Math.round(x); // gives the down numbers nearest to the integer
+    //z = Math.ceil(x); // gives the high integer numbers 
+        // z = Math.log(x); // it gives the lowest numbers integer
+        // z = Math.trunc(x);// it give the integer not in the decimal or in fraction forms
+        // z = Math.pow(x,y); // it gives the power 
+        // x = Math.sqrt(x,y); //it give the square root
+        // z = Math.log(x); // it gives the lowest integer 
+        // z = Math.cos(x); // it gives the integer in the negative
+        // z = Math.tan(x);  // it also give the integers in teh minus 
+        // z = Math.abs(x); // it gives the absolute integer
+        // z = Math.sign(x); // it also work as an integer but in it works in the decimal 
+        // z = Math.max (s,x,y);  //it will help to find the max numbers or integers
+        // z = Math.min(s,x,y);  it will help to find the medium integers or numbers
+        console.log(z); 
